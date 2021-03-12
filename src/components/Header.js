d5a66d91b52aa46ca3e256f046b61f2758e67205
garcialuis <garcialuisdev@gmail.com>
2021-03-11 21:03:51 -0800

    feat: Added initial files for Header and NutriportApp components
    The files for the Header and NutriportApp components have been
    started. The directory where styles will live have also been
    included in this commit.

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <button>Nav</button>
+            <h2>Nutriport</h2>
+            <button>ProfileButton</button>
+        </div>
+    )
+}
+
+export default Header
