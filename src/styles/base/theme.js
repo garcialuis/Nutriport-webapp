c5950277aa63dbf52d8ebf2b5684a951d0118848
garcialuis <garcialuisdev@gmail.com>
2021-03-16 14:58:40 -0700

    refactor: Modified theme.js to be able to reuse theme colors
    src/styles/base/theme.js has been modified - the primary and secondary
    colors of the theme have been initialized before the custom theme
    to be able to reuse them throughout our custom theme.

--- src/styles/base/theme.js
+++ src/styles/base/theme.js
@@ -1,23 +1,38 @@
 // import { createMuiTheme } from '@material-ui/core/styles';
 import { unstable_createMuiStrictModeTheme as createMuiTheme } from '@material-ui/core';
 
+// Theme Colors:
+const primaryMain = '#8bc34a',
+    primaryLight = '#a2cf6e',
+    primaryDark = '#618833';
+
+const secondaryMain = '#ffc400',
+    secondaryLight = '#ffcf33',
+    secondaryDark = '#b28900';
 
 export const theme = createMuiTheme({
 
     palette: {
         primary: {
-            light: '#a2cf6e',
-            main: '#8bc34a',
-            dark: '#618833'
+            light: primaryLight,
+            main: primaryMain,
+            dark: primaryDark
         },
         secondary: {
-            light: '#ffcf33',
-            main: '#ffc400',
-            dark: '#b28900'
+            light: secondaryLight,
+            main: secondaryMain,
+            dark: secondaryDark
         }
     },
     typography: {
         fontFamily: 'Georgia'
+    },
+    overrides: {
+        MuiMenu: {
+            paper: {
+                background: secondaryMain
+            }
+        }
     }
 
 });
